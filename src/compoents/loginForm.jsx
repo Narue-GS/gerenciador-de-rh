d25ec5e030c3e4bf08621388b71d9273f6e5ce8b
Narue-GS <naruegit@gmail.com>
2023-03-24 11:05:52 -0300

    feat: add login and logout functions, using local storage.

--- src/compoents/loginForm.jsx
+++ src/compoents/loginForm.jsx
@@ -1,17 +1,23 @@
 import '../styles/loginform.css'
 
-const LoginForm = () => {
+const LoginForm = ({loginFunc}) => {
   return(
     <>
     <h1>Login</h1>
     
       <div className='corpo'>
-        <form action="" method="post">
           <p>E-mail</p>
-          <input placeholder="Usuário"/>
+          <input id="userEmail" placeholder="Email"/>
           <p>Senha</p>
-          <input placeholder="Senha"/>
-        </form>
+          <input id="userPassword" placeholder="Senha"/>
+					<button onClick={() =>{
+						const thisUser = {
+							email: document.querySelector("input#userEmail").value, 
+							password:document.querySelector("input#userPassword").value
+						}
+						loginFunc(thisUser)
+					}
+				}>Continuar</button>
       </div>
     </>
   )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,38 @@ import LoginForm from "../compoents/loginForm"
 import { useState } from "react"
 
 const Home = () => {
-  const [permissions, setPermissions] = [{id:0, name:"read"}, {id:1}]
-  const [jurisdictions , setJurisdictions] = useState([{id:0, name:"Gerente", permissions:[0,1,2]}])
-  const [sersList, setUserList] = useState([{name:"Carlos", jurisdiction:0}])
-  return(
-    <LoginForm />
-  )
+  const [permissions, setPermissions] = useState([
+    {id:0, name:"Ver usuários"},
+    {id:1, name:"Contratar"},
+    {id:2, name:"Demitir"},
+    {id:3, name:"Modificar usuários"},
+		{id:4, name:"Cadastrar, modificar e deletar alçadas"},
+		{id:5, name:"Cadastrar, modificar e deletar permições"}
+  ])
+	const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("currentUser")))
+
+	const login = (outUser) => {
+		usersList.map((user) => { 
+			if(user.password === outUser.password && user.email === outUser.email){
+				localStorage.setItem("currentUser", JSON.stringify(user))
+				setCurrentUser(user)
+				alert("logado com sucesso")
+				return true
+			}
+		})
+	}
+	const logout = () =>{
+		localStorage.removeItem("currentUser")
+		setCurrentUser(null)
+	}
+  
+  const [jurisdictions , setJurisdictions] = useState([{id:0, name:"Gerente", permissions:[0,1,2,3,5]}])
+  const [usersList, setUserList] = useState([{id:0, name:"Carlos", email:"carlos@gmail.com", password:"wE4&34e$5$Ix", age:null, jurisdiction:0}])
+	return(
+		<>
+			{currentUser? <h1 onClick={logout}>{currentUser.name}</h1> : <LoginForm loginFunc={login}/>}
+  	</>
+	)
 }
 
 export default Home
