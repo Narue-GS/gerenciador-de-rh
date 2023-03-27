f5ad38805e92baf1a8fe1133a36c6a1956e3f2f3
Victor Edilmo Cardoso santos <victor2017tj@gmail.com>
2023-03-27 15:28:17 -0300

    atualizan dologin

--- src/compoents/loginForm.jsx
+++ src/compoents/loginForm.jsx
@@ -5,22 +5,29 @@ const LoginForm = ({loginFunc}) => {
     <>
     <div className='corpo'>
 
-      <h1 className='texto'>Login</h1>
+    <div className='login'>
       
-        <div className='login'>
+      <h1 className='texto'>Login</h1>
+
+          <div>
             <h1>E-mail</h1>
             <input id="userEmail" placeholder="Email"/>
+          </div>
+
+          <div>
             <h1>Senha</h1>
             <input id="userPassword" placeholder="Senha"/>
-                <button className='botão' onClick={() =>{
-                  const thisUser = {
-                    email: document.querySelector("input#userEmail").value, 
-                    password:document.querySelector("input#userPassword").value
-                  }
-                  loginFunc(thisUser)
+          </div>
+
+              <button onClick={() =>{
+                const thisUser = {
+                  email: document.querySelector("input#userEmail").value, 
+                  password:document.querySelector("input#userPassword").value
                 }
-              }>Continuar</button>
-        </div>
+                loginFunc(thisUser)
+              }
+            }>Continuar</button>
+      </div>
     </div>
     </>
   )
