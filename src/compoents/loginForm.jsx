90aeaaebd4827a403724d3320e069df6bfdab5ca
Victor Edilmo Cardoso santos <victor2017tj@gmail.com>
2023-03-27 12:51:00 -0300

    add icones na tela

--- src/compoents/loginForm.jsx
+++ src/compoents/loginForm.jsx
@@ -3,22 +3,25 @@ import '../styles/loginform.css'
 const LoginForm = ({loginFunc}) => {
   return(
     <>
-    <h1>Login</h1>
-    
-      <div className='corpo'>
-          <p>E-mail</p>
-          <input id="userEmail" placeholder="Email"/>
-          <p>Senha</p>
-          <input id="userPassword" placeholder="Senha"/>
-					<button onClick={() =>{
-						const thisUser = {
-							email: document.querySelector("input#userEmail").value, 
-							password:document.querySelector("input#userPassword").value
-						}
-						loginFunc(thisUser)
-					}
-				}>Continuar</button>
-      </div>
+    <div className='corpo'>
+
+      <h1 className='texto'>Login</h1>
+      
+        <div className='login'>
+            <h1>E-mail</h1>
+            <input id="userEmail" placeholder="Email"/>
+            <h1>Senha</h1>
+            <input id="userPassword" placeholder="Senha"/>
+                <button className='botão' onClick={() =>{
+                  const thisUser = {
+                    email: document.querySelector("input#userEmail").value, 
+                    password:document.querySelector("input#userPassword").value
+                  }
+                  loginFunc(thisUser)
+                }
+              }>Continuar</button>
+        </div>
+    </div>
     </>
   )
 }
