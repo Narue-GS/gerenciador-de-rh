ef9ac1b33f0cf27e93bc8a25e0048572dcb4749e
Narue-GS <naruegit@gmail.com>
2023-03-30 08:39:39 -0300

    feat: create modal for user registration

---
+++ src/components/modalUserRegister.jsx
@@ -0,0 +1,14 @@
+const ModalUserRegister = () => {
+	return (
+		<div className="modal">
+			<div className="modal-shadow">
+			</div>
+			<div id="modal-content">
+				<div>
+					<input></input>
+					<input></input>
+				</div>
+			</div>
+		</>
+	)
+}
