e7537ea7d5b5da66dac5dbb07ba0fe285aaeb192
Victor Edilmo Cardoso santos <118738444+Victor-E-C-Santos@users.noreply.github.com>
2023-04-11 16:49:18 -0300

    arrumando a barra de rolagem

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -8,21 +8,34 @@ const Welcome = () => {
     <>
         
             <div className='circle1'><i class="fa-solid fa-circle"></i></div>
+
             <div className='circle5'>
                 <GiCircle color='#4e5a9e' fontSize="17vw"/>
             </div>
+
             <div className='circle6'>
                 <GiCircle color='#4e5a9e' fontSize="20vw"/>
             </div>
-
-
-                <div id="circle2"></div>
+            
+            <div id="circle2"></div>
 
                 <div id="circle3"></div>
+                
                 <hr className='line top1'/>
+
                 <hr className='line top2'/>
+
                 <hr className='line bottom1'/>
+
                 <hr className='line bottom2'/>
+
+                <div className='textprincipal'>
+                    <h1 className='texth1'>BEM-VINDO</h1>
+                </div>
+
+                <div className='textp'>
+                    <p>By: Logical Thinking</p>
+                </div>
     </>
     )
 }
